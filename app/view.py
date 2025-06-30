78d0ce7fb922bbab2030adfce0195cdf65ebff4e
Kotry <gustavokotryk@gmail.com>
2025-06-30 08:08:53 -0300

    fix: Resolvendo o erro do dicionario

---
+++ app/view.py
@@ -0,0 +1,19 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+def homepage():
+    nome = "kotryk"
+    idade = 17
+    interesses = ["Programação", "Música", "Jogos"]
+
+    dicionario = {
+        'nome': nome,
+        'idade': idade,
+        'interesses': interesses
+    }
+    return render_template('index.html', dicionario=dicionario)
+
+@app.route('/sobre/')
+def sobre():
+    return render_template('sobre.html')
